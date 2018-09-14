df2122dd0f1a6ea2e3b8f7e36d974b88c3498b53
vincent <vincent@absolutlabs.com>
2018-09-14 15:46:27 +0200

    carthage support

---
+++ Underscore_m/Underscore_m.h
@@ -0,0 +1,19 @@
+//
+//  Underscore_m.h
+//  Underscore_m
+//
+//  Created by Vincent Porret on 14/09/2018.
+//  Copyright © 2018 Robert Böhnke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Underscore_m.
+FOUNDATION_EXPORT double Underscore_mVersionNumber;
+
+//! Project version string for Underscore_m.
+FOUNDATION_EXPORT const unsigned char Underscore_mVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Underscore_m/PublicHeader.h>
+
+
